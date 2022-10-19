cd21b9ba9c91cbe9606d4786356af79104ee2335
Raphael Camilo <raphael332244@gmail.com>
2022-10-19 10:08:08 -0300

    added storybook

---
+++ src/components/Main/stories.tsx
@@ -0,0 +1,9 @@
+import { ComponentStory } from "@storybook/react"
+import Main from "."
+
+export default {
+  title: "Main",
+  component: <Main />
+}
+
+export const Basic: ComponentStory<typeof Main> = () => <Main />
